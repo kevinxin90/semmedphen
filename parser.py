5638205a2d8695bfb2fe06de8d89b67cb5117fde
jiwen xin <kevinxin@scripps.edu>
2019-08-30 10:38:04 -0700

    add name field

--- parser.py
+++ parser.py
@@ -26,7 +26,7 @@ def load_data(data_folder):
         next(csv_reader)
         for _item in csv_reader:
             group_by_semmantic_dict[_item[-2]].append(_item[-1])
-            id_type_mapping[_item[-1]] = _item[-2]
+            id_type_mapping[_item[-1]] = {'type': _item[-2], 'name': _item[1]}
     phenotype_related = {}
     parsed_type_list = ['disease_or_phenotypic_feature', 'gene',
                         'protein', 'chemical_substance']
@@ -37,9 +37,10 @@ def load_data(data_folder):
             if _item[4] in group_by_semmantic_dict['phenotypic_feature']:
                 if _item[4] not in phenotype_related:
                     phenotype_related[_item[4]] = {'_id': _item[4][5:],
-                                                  'umls': _item[4][5:]}
+                                                  'umls': _item[4][5:],
+                                                  'name': id_type_mapping[_item[4]]['name']}
                 pred = _item[0].lower()
-                semantic_type = id_type_mapping[_item[5]]
+                semantic_type = id_type_mapping[_item[5]]['type']
                 if semantic_type not in parsed_type_list:
                     if pred not in phenotype_related[_item[4]]:
                         phenotype_related[_item[4]][pred] = {}
@@ -49,9 +50,10 @@ def load_data(data_folder):
             elif _item[5] in group_by_semmantic_dict['phenotypic_feature']:
                 if _item[5] not in phenotype_related:
                     phenotype_related[_item[5]] = {'_id': _item[5][5:],
-                                                  'umls': _item[5][5:]}
+                                                  'umls': _item[5][5:],
+                                                  'name': id_type_mapping[_item[5]]['name']}
                 pred = _item[0].lower() + '_reverse'
-                semantic_type = id_type_mapping[_item[4]]
+                semantic_type = id_type_mapping[_item[4]]['type']
                 if semantic_type not in parsed_type_list:
                     if pred not in phenotype_related[_item[5]]:
                         phenotype_related[_item[5]][pred] = {}
