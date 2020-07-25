7e56c01cc03a1f4ea990efe2f49d3c556a699178
jiwen xin <kevinxin@scripps.edu>
2020-07-24 20:44:41 -0700

    update parser

--- parser.py
+++ parser.py
@@ -170,5 +170,9 @@ def load_data(data_folder):
     for v in gene_related.values():
         for m, n in v.items():
             if m not in ["_id", "umls", "name", "@type"]:
-                v[m] = n.values()
+                tmp = []
+                for item in n.values():
+                    item['pmid'] = list(item['pmid'])
+                    tmp.append(item)
+                v[m] = tmp
         yield v
